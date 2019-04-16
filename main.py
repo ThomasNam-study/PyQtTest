dd6538aef0ea0b76f612d3576381db8893f9b95d
RED <purred@udid.co.kr>
2019-04-16 11:34:27 +0900

    위젯 작업 완료

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import sys
 
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow, QAction, qApp
+from PyQt5.QtCore import QCoreApplication, QDate, Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMainWindow, QAction, qApp, QDesktopWidget, \
+    QLabel, QVBoxLayout
 from PyQt5.QtGui import QIcon, QFont
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        lbl_red = QLabel("Red")
+        lbl_green = QLabel("Green")
+        lbl_blue = QLabel("Blue")
+
+        lbl_red.setStyleSheet("color: red;"
+                              "border-style: solid;"
+                              "border-width: 2px;"
+                              "border-color: #FA8072;"
+                              "border-radius: 3px"
+                              )
+
+        layout = QVBoxLayout()
+        layout.addWidget(lbl_red)
+        layout.addWidget(lbl_green)
+        layout.addWidget(lbl_blue)
+        # layout.addWidget(btn)
+
+        self.setLayout(layout)
+        self.setGeometry(600, 300, 600, 600)
+
+
+
 
 
 class MyApp(QMainWindow):
@@ -17,8 +44,9 @@ class MyApp(QMainWindow):
         exitAction.setStatusTip("Exit application")
         exitAction.triggered.connect(qApp.quit)
 
+        date = QDate.currentDate()
 
-        self.statusBar().showMessage("Ready")
+        self.statusBar().showMessage(date.toString(Qt.DefaultLocaleLongDate))
 
         menubar = self.menuBar()
 
@@ -27,22 +55,35 @@ class MyApp(QMainWindow):
         fileMenu = menubar.addMenu("&File")
         fileMenu.addAction(exitAction)
 
+        self.toolbar = self.addToolBar("Exit")
+        self.toolbar.addAction(exitAction)
+
         QToolTip.setFont(QFont("SansSerif", 10))
         self.setToolTip("This is a <B>QWidget</B> widget")
 
-        btn = QPushButton("Quit", self)
-        btn.setToolTip("This is a <B>QPushButton</B> widget")
-
-        btn.move(50, 50)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(QCoreApplication.instance().quit)
+        # btn = QPushButton("Quit")
+        # btn.setToolTip("This is a <B>QPushButton</B> widget")
+        #
+        # btn.move(50, 100)
+        # btn.resize(btn.sizeHint())
+        # btn.clicked.connect(QCoreApplication.instance().quit)
+        # self.move(300, 300)
+        #self.resize(400, 200)
 
         self.setWindowTitle("My First Application")
         self.setWindowIcon(QIcon("web.png"))
-        # self.move(300, 300)
-        #self.resize(400, 200)
-        self.setGeometry(600, 300, 600, 600)
+
+        self.setCentralWidget(MyWidget())
+        self.setGeometry(600, 600, 600, 600)
+        self.center()
         self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
